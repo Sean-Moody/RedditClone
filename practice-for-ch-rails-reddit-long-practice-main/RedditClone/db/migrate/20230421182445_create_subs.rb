f6d64e6fb25f8a16c85cd2b0c4fcba099001e047
joshua <joshk.5793@gmail.com>
2023-04-21 15:33:50 -0400

    added controllers

---
+++ practice-for-ch-rails-reddit-long-practice-main/RedditClone/db/migrate/20230421182445_create_subs.rb
@@ -0,0 +1,13 @@
+class CreateSubs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subs do |t|
+      t.string :title, null: false
+      t.text :description
+      t.bigint :moderator_id, null: false
+      t.timestamps
+    end
+    add_index :subs, :title
+    
+    add_foreign_key :subs, :users, column: :moderator_id
+  end
+end
--- practice-for-ch-rails-reddit-long-practice-main/RedditClone/db/schema.rb
+++ practice-for-ch-rails-reddit-long-practice-main/RedditClone/db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_175308) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_182445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subs", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.bigint "moderator_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_subs_on_title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_175308) do
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "subs", "users", column: "moderator_id"
 end
